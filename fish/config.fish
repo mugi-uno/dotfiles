e1e60e6fdaa98561e1ccf07e55f3362bbb9b5925
mugi-uno <mugi.uno@gmail.com>
2021-04-15 16:30:41 +0900

    update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -4,14 +4,17 @@
 # omf install bobthefish
 # omf install peco
 
-set -g theme_powerline_fonts no
-set -g theme_nerd_fonts yes
 set -g theme_color_scheme terminal-dark
 set -g theme_newline_cursor no
 set -g theme_display_date no
+set -g theme_show_exit_status no
 
 function fish_user_key_bindings
   bind \cr peco_select_history # Bind for prco history to Ctrl+r
 end
 
 set GHQ_SELECTOR peco
+
+function fish_right_prompt
+  #intentionally left blank
+end
